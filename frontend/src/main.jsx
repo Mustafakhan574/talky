e1b84e9a8a38bc17d3100f9b91798c973322e59e
Mustafakhan574 <mk3855315@gmail.com>
2025-07-26 17:50:28 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
-export const serverurl = "http://localhost:3000"
+export const serverurl = "https://talky-backend-46fw.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
